b4a4f43ccf312e8df0f467cdcc6fd24249465a49
Vitor Oliveira <151887959+VitorSOliveira-eng@users.noreply.github.com>
2026-05-25 18:09:38 -0300

    Update Sistema.java

--- src/Sistema.java
+++ src/Sistema.java
@@ -1,16 +1,53 @@
 public class Sistema {
+
     private static Sistema instance;
-    private Disciplina disciplina;
-    private Turma turma;
-    private Aluno aluno;
-    public Sistema() {}
-    public static Sistema getInstance(){
+
+    private Disciplina[] disciplinas;
+    private Turma[] turmas;
+    private Aluno[] alunos;
+    private Matricula[] matriculas;
+
+    private Sistema() {
+
+        disciplinas = new Disciplina[100];
+        turmas = new Turma[100];
+        alunos = new Aluno[100];
+        matriculas = new Matricula[100];
+    }
+
+    public static Sistema getInstance() {
+
         if (instance == null) {
-            instance = new Sistema(); 
+            instance = new Sistema();
         }
-        return instance; 
-    }
+
+        return instance;
     }
 
-   
+    public boolean cadastradisc(Disciplina disc) {
+        if (disc != null) {
+            for (int i = 0; i < disciplinas.length; i++) {
+                if (disciplinas[i].getCddisciplina() == disc.getCddisciplina()) {
+                    return false;
+                }
+            }
+            for (int i = 0; i < disciplinas.length; i++) {
+                if (disciplinas[i] == null) {
+                    disciplinas[i] = disc;
+                    return true;
+                }
+            }
 
+        }
+        return false;
+    }
+
+    public boolean cadastraaluno(Aluno aluno){
+        if(aluno != null){
+            for(int i = 0; i<alunos.length;i++){
+                if()
+            }
+        }
+    }
+
+}
