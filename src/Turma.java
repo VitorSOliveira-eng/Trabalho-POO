4cb729d6ff71295cbd4412546e9d6ece72677c71
Mateuscrvc <mateuscarvalhocosta.mc@gmail.com>
2026-05-25 16:51:50 -0300

    Update Turma.java

--- src/Turma.java
+++ src/Turma.java
@@ -1,8 +1,30 @@
 public class Turma {
     private int ano;
     private int numerovagas;
-    private Disciplina disciplinas[];
-    private Matricula matriculas[];
+    private Disciplina[] disciplinas;
+    private Matricula[] matriculas;
+    
+    
+    private Turma(int ano, int numerovagas) {
+        
+        if(ano != 0 && numerovagas != 0 && disciplinas != null && matriculas != null){
+            this.ano = ano;
+            this.numerovagas = numerovagas;
+            this.disciplinas = new Disciplina[10];
+            this.matriculas = new Matricula[10];
+        }
+    }
+    
+    public static Turma criarTurma(int ano, int numerovagas) {
+        if(ano != 0 && numerovagas > 0) {
+            return new Turma(ano, numerovagas);
+        } else {
+            return null;
+        }
+
+
+
+    }
     
     //private Disciplina disciplina;
     //private Aluno alunosmatriculados;
