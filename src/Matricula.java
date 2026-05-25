84b05ef3ab14633ff5e9f8642264e125f564f5c6
Mateuscrvc <mateuscarvalhocosta.mc@gmail.com>
2026-05-25 16:27:23 -0300

    Update Matricula.java

--- src/Matricula.java
+++ src/Matricula.java
@@ -1,5 +1,4 @@
 
-
 import java.sql.Date;
 
 public class Matricula {
@@ -7,7 +6,31 @@ public class Matricula {
     private Turma turma;
     private Aluno aluno;
 
-    
+    private Matricula(Date data, Turma turma, Aluno aluno) {
+        this.data = data;
+        this.turma = turma;
+        this.aluno = aluno;
+    }
+
+    public static Matricula criarMatricula(Date data, Turma turma, Aluno aluno){
+        if(data != null && turma != null && aluno != aluno) {
+            return new Matricula(data,turma,aluno);
+        } else {
+            return null;
+        }
+    }  
+
+    public Date getData() { 
+        return data; 
+    }
+    public Turma getTurma() { 
+        return turma; 
+    }
+    public Aluno getAluno() { 
+        return aluno; 
+    }
+
+
 
 
 }
