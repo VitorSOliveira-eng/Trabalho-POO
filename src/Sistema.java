1b90e5d1365ad690d0fca6b0cb54eece03cd877a
Vitor Oliveira <151887959+VitorSOliveira-eng@users.noreply.github.com>
2026-05-25 18:38:51 -0300

    Update Sistema.java
    metodos de cadastro

--- src/Sistema.java
+++ src/Sistema.java
@@ -27,7 +27,7 @@ public class Sistema {
     public boolean cadastradisc(Disciplina disc) {
         if (disc != null) {
             for (int i = 0; i < disciplinas.length; i++) {
-                if (disciplinas[i].getCddisciplina() == disc.getCddisciplina()) {
+                if (disciplinas[i] != null && disciplinas[i].getCddisciplina() == disc.getCddisciplina()) {
                     return false;
                 }
             }
@@ -42,12 +42,55 @@ public class Sistema {
         return false;
     }
 
-    public boolean cadastraaluno(Aluno aluno){
-        if(aluno != null){
-            for(int i = 0; i<alunos.length;i++){
-                if()
+    public boolean cadastraaluno(Aluno aluno) {
+        if (aluno != null) {
+            for (int i = 0; i < alunos.length; i++) {
+                if (alunos[i] != null && alunos[i].getNome().equals(aluno.getNome())) {
+                    return false;
+                }
+            }
+            for (int i = 0; i < alunos.length; i++) {
+                if (alunos[i] == null) {
+                    alunos[i] = aluno;
+                    return true;
+                }
             }
         }
+        return false;
+    }
+
+    public boolean cadastraturma(Turma turma) {
+        if (turma != null) {
+            for (int i = 0; i < turmas.length; i++) {
+                if (turmas[i] != null && turmas[i].getAno() == turma.getAno()) {
+                    return false;
+                }
+            }
+            for (int i = 0; i < turmas.length; i++) {
+                if (turmas[i] == null) {
+                    turmas[i] = turma;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean cadastramatricula(Matricula matricula){
+        if(matricula != null){
+            for(int i =0; i<matriculas.length; i++){
+                if(matriculas[i] !=null && matriculas[i].getAluno()== matricula.getAluno()){
+                    return false;
+                }
+            }
+            for(int i =0; i<matriculas.length; i++){
+                if(matriculas[i] == null){
+                    matriculas[i] = matricula;
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
 }
