628435fac3766a2ade9dcb06747f3cfd3a8d29c1
Vitor Oliveira <151887959+VitorSOliveira-eng@users.noreply.github.com>
2026-05-26 17:47:34 -0300

    Update App.java
    so pra testar a listagem e o metodo init

--- src/App.java
+++ src/App.java
@@ -1,6 +1,16 @@
-
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Sistema sistema = Sistema.getInstance();
+        sistema.init(); // <--- Adicione essa linha aqui!
+        
+        // Agora chame o seu método de listar para ver a mágica acontecer:
+        System.out.println("--- ALUNOS PRÉ-CADASTRADOS NO INIT ---");
+        sistema.listarAlunos();
+        
+        System.out.println("\n--- DISCIPLINAS PRÉ-CADASTRADAS NO INIT ---");
+        sistema.listarDisciplinas();
+
+        System.out.println("Turmas pre cadastradas");
+        sistema.listarTurma();
     }
 }
